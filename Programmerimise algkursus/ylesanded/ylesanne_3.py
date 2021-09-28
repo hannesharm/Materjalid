9b0020400dc9352148143f550af51fcccf432998
Hannes <haharm@ttu.ee>
2021-09-28 18:09:44 +0300

    4. nädal fix

--- Programmerimise algkursus/ylesanded/ylesanne_3.py
+++ Programmerimise algkursus/ylesanded/ylesanne_3.py
@@ -1,12 +1,22 @@
 def count(a):
     """
     TODO: enne lahendamist
-    Leia youtubest for loopide kohta tutorial, vaata läbi ja ürita aru saada
+    Vaata läbi video
+    https://www.youtube.com/watch?v=OnDr4J2UXSA
     Kui ei saa aru, vaata teisi videoid/loe dokumentatsiooni vms.
 
     TODO count all letters in parameter a
     Put each found letter into dictionary
     as key and value must be the amount of letters found
+
+    Example: "kalamaja majas" =>
+    {
+        'k': 1,
+        'a': 6,
+        'l': 1,
+        'm': 2,
+        's': 1
+    }
     """
     return {'k': 2, 'a': 1, 'u': 1, 's': 2}
 
